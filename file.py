09701de1820703600c5899744a06330137676050
lxhdj0215 <wangguijun.123@163.com>
2018-05-05 19:27:18 +0800

    练习

--- file.py
+++ file.py
@@ -1,7 +1,18 @@
+# encoding=utf8
 file = open("D:\\1.txt", "r")
 
 for line in file.readlines():
     if line.__contains__("Football"):
         print(line)
 
+file.close()
+
+file = open("D:\\w.txt", "w")
+for i in range(100):
+    file.write("王桂军" + str(i))
+    file.write("\n")
+file.close()
+
+file = open("D:\\w.txt", "r")
+print(file.read())
 file.close()
